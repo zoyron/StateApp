e2ed98cd01702df223bf28e217ba7e509ed0336b
zoyron <sgrarora3@gmail.com>
2020-04-14 03:40:18 +0530

    completed the assignment as well

--- src/App.js
+++ src/App.js
@@ -13,8 +13,8 @@ const App = () => {
       </header>
       <h2>Current value of count is {count}</h2>
       <button onClick={() => setCount(0)}>Reset Counter</button>
-      <button onClick={() => setCount(count + 1)}>Increase Counter</button>
-      <button onClick={() => setCount(count - 1)}>Decrease Counter</button>
+      <button onClick={() => (count <= 9 ? setCount(count+1) : "")}>Increase Counter</button>
+      <button onClick={() => (count > 0 ? setCount(count-1) : "")}>Decrease Counter</button>
     </div>
   )
 }
