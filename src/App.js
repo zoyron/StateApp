965c65b6dc41e76e9e773e5102a0a2044029a7c3
zoyron <sgrarora3@gmail.com>
2020-04-14 03:34:44 +0530

    the state app is completed (before assignment part)

--- src/App.js
+++ src/App.js
@@ -1,26 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react';
+import "./App.css";
 
-function App() {
-  return (
+
+const App = () => {
+  const [count, setCount] = useState(0); // anything that we put up in the useState brackets is the default value of the count variable
+
+
+  return(
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <h1>Counter app using state</h1>
       </header>
+      <h2>Current value of count is {count}</h2>
+      <button onClick={() => setCount(0)}>Reset Counter</button>
+      <button onClick={() => setCount(count + 1)}>Increase Counter</button>
+      <button onClick={() => setCount(count - 1)}>Decrease Counter</button>
     </div>
-  );
+  )
 }
 
 export default App;
